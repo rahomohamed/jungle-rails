3a89cebfddf96e0e2443220d58d92c9fb539b71b
rahomohamed <rahojmohamed@gmail.com>
2020-04-06 06:55:13 +0000

    Bug: Admin Security

--- app/controllers/concerns/http_auth_concern.rb
+++ app/controllers/concerns/http_auth_concern.rb
@@ -3,6 +3,7 @@ module HttpAuthConcern
   included do
       before_action :http_authenticate
   end
+  
   def http_authenticate
       return true unless Rails.env == 'production'
       authenticate_or_request_with_http_basic do |username, password|
